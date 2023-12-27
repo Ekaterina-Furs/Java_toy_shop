57b1a9d6d3614b522809c193f5b399f3e09e3eb3
EkaterinaFurs <mrs-katrina@mail.ru>
2023-12-27 12:10:10 +0400

    Add_methods_file

---
+++ ToyShop.java
@@ -0,0 +1,54 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import java.util.PriorityQueue;
+
+public class ToyShop implements Iterable<Toy> {
+    private PriorityQueue<Toy> toyQueue = new PriorityQueue<>();
+
+    // Метод добавления игрушки в магазин
+    public void addToy(Toy toy) {
+        toyQueue.add(toy);
+    }
+
+    // Метод для получения игрушки из магазина (извлечения с наивысшей частотой)
+    public Toy getToy() {
+        return toyQueue.poll();
+    }
+
+    // Метод для рандомизации и записи в файл, а также вывода на консоль
+    public void organizeRaffle(int iterationCount) {
+        // проверяем наличие файла
+        File file = new File("Java_Attest/output.txt");
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        try (FileWriter writer = new FileWriter(file)) {
+            for (int i = 0; i < iterationCount; i++) {
+                // double random = Math.random() * 100;
+                int random = ((int) Math.floor(Math.random() * 100)) + 1;
+                for (Toy toy : toyQueue) {
+                    if (random < toy.getFrequency()) {
+                        System.out.println(toy);
+                        writer.write(String.format("Выпала игрушка: %s (ID: %d)\n", toy.getName(), toy.getId()));
+                    }
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public Iterator<Toy> iterator() {
+        return toyQueue.iterator();
+    }
+
+}
